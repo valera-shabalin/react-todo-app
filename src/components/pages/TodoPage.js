81c7c17508758532318ba3d5d63ba5b59b1b394b
Valera Shabalin <shabalinvalera.ts@gmail.com>
2021-06-06 18:17:02 +0700

    Update TodoPage

--- src/components/pages/TodoPage.js
+++ src/components/pages/TodoPage.js
@@ -1,20 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import { Container, Grid } from "@material-ui/core";
+import {
+    Container,
+    Grid,
+    Paper,
+    Checkbox,
+    makeStyles
+} from "@material-ui/core";
+
+import { Pagination } from '@material-ui/lab';
+
+const useStyle = makeStyles({
+    title: {
+        marginBottom: 25
+    },
+    item: {
+        padding: 20
+    }
+});
 
 export default function TodoPage() {
+    const [todos, setTodos] = useState([
+        {
+            title: "Дело",
+            id: 1
+        },
+        {
+            title: "Дело 2",
+            id: 2
+        },
+        {
+            title: "Дело 3",
+            id: 3
+        },
+        {
+            title: "Дело 4",
+            id: 4
+        }
+    ]);
+    const classes = useStyle();
+
     return (
         <Container>
-            <Grid item xs={12}>
-                <h1>Списки дел</h1>
-            </Grid>
-            <Grid item xs={12}>
-                <Link to="/todo/task/1">1</Link>
-                <Link to="/todo/task/2">2</Link>
-                <Link to="/todo/task/3">3</Link>
-                <Link to="/todo/task/4">4</Link>
-                <Link to="/todo/task/5">5</Link>
+            <Grid
+                container
+                direction="column"
+            >
+                <Grid item className={classes.title}>
+                    <h1>Списки дел</h1>
+                </Grid>
+                <Grid
+                    container
+                    direction="column"
+                    spacing={2}
+                >
+                    {
+                        todos.map(item => {
+                            return (
+                                <Grid item key={item.id}>
+                                    <Paper
+                                        variant="outlined"
+                                        square
+                                        className={classes.item}
+                                    >
+                                        <Checkbox color="primary"/>
+                                        <Link to={`/todo/task/${item.id}`}>{item.title}</Link>
+                                    </Paper>
+                                </Grid>
+                            );
+                        })
+                    }
+                </Grid>
             </Grid>
         </Container>
     );
